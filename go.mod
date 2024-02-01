9c6087e05aafd54e61a5e6bdd399a5d1e4f7349e
kou12345 <hi.0412.ko.2002@gmail.com>
2024-02-01 17:12:53 +0900

    ファイル表示とコードハイライト機能を実装

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/kou12345/go-cat
+
+go 1.21.6
+
+require github.com/alecthomas/chroma v0.10.0
+
+require github.com/dlclark/regexp2 v1.4.0 // indirect
---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/alecthomas/chroma/formatters"
+	"github.com/alecthomas/chroma/lexers"
+	"github.com/alecthomas/chroma/styles"
+)
+
+// ファイルの内容をハイライトして返す関数
+func highlightCode(code string) (string, error) {
+	// TODO とりあえず、Goのみ ファイルの拡張子によってlexerを選択する
+	lexer := lexers.Get("go")
+	iterator, err := lexer.Tokenise(nil, code)
+	if err != nil {
+		return "", err
+	}
+
+	style := styles.Get("nord")
+	if style == nil {
+		style = styles.Fallback
+	}
+
+	formatter := formatters.Get("terminal256")
+	if formatter == nil {
+		formatter = formatters.Fallback
+	}
+
+	var b bytes.Buffer
+	err = formatter.Format(&b, style, iterator)
+	if err != nil {
+		return "", err
+	}
+	return b.String(), nil
+
+}
+
+func cat(r *bufio.Reader) {
+	for {
+		// 改行文字が見つかるまで読み込む
+		buf, err := r.ReadBytes('\n')
+		if err == io.EOF {
+			break
+		}
+
+		highlighted, err := highlightCode(string(buf))
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error highlighting code:", err)
+			continue
+		}
+		// 標準出力に書き込む
+		fmt.Fprintf(os.Stdout, "%s", highlighted)
+	}
+}
+
+func main() {
+	// コマンドライン引数を解析
+	flag.Parse()
+
+	if flag.NArg() == 0 {
+		// 引数がない場合は標準入力を表示
+		cat(bufio.NewReader(os.Stdin))
+	}
+
+	for i := 0; i < flag.NArg(); i++ {
+		// ファイルを開く
+		f, err := os.Open(flag.Arg(i))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s: error reading from %s: %s\n", os.Args[0], flag.Arg(i), err)
+			continue
+		}
+		// ファイルの内容を表示
+		cat(bufio.NewReader(f))
+		f.Close()
+	}
+}
